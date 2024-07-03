792968c77e4be953b60bed2730182ce4e1f268b1
mohiiit <mohit18343@iiitd.ac.in>
2024-07-03 13:53:20 +0530

    declare and deploy eth contract

--- scripts/test_script.js
+++ scripts/test_script.js
@@ -1,5 +1,6 @@
 const starknet = require("starknet");
 const sierra = require("../target/dev/pitch_lake_starknet_Eth.contract_class.json");
+const casm = require("../target/dev/pitch_lake_starknet_Eth.compiled_contract_class.json");
 
 async function main(port) {
   const provider = new starknet.RpcProvider({
@@ -8,19 +9,25 @@ async function main(port) {
   const account = new starknet.Account(
     provider,
     "0x4d75495e10ee26cae76478b6e491646ff0a10e0a062db1555131e47b07b7d24",
-    "0x100801800000000310080180000000010030000000000005106801800206800",
-    "1"
+    "0x100801800000000310080180000000010030000000000005106801800206800"
   );
   console.log("port here is: ", port);
+
+  try {
+    const declareResult = await account.declare({
+      contract: sierra,
+      casm: casm,
+    });
+    console.log("This is the declare result - ", declareResult);
+  } catch (err) {
+    console.log("Contract is already declared");
+  }
 
   let constructorArgs = [
     1,
     0,
     "0x00c1cf1490de1352865301bb8705143f3ef938f97fdf892f1090dcb5ac7bcd1d",
   ];
-  //   if (process.argv.length > 3) {
-  //     constructorArgs = process.argv.slice(3);
-  //   }
 
   const deployResult = await account.deploy({
     classHash: starknet.hash.computeContractClassHash(sierra),
