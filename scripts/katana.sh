cb1ae99ec88c41a18740a022bef8d5adf771acd6
mohiiit <mohit18343@iiitd.ac.in>
2024-07-18 04:10:13 +0530

    mine next block issue fixed

--- scripts/katana.sh
+++ scripts/katana.sh
@@ -4,7 +4,7 @@
 PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
 echo "Starting katana on port $PORT"
-katana --chain-id SN_SEPOLIA --host 127.0.0.1 --port 5050 --accounts "25" --seed "1" -b 2000 &
+katana --chain-id SN_SEPOLIA --host 127.0.0.1 --port 5050 --accounts "25" --seed "1" -b 2000 --dev &
 
 while ! nc -z localhost $PORT; do   
   sleep 0.1 # wait for 1/10 of the second before check again
--- scripts/utils/helpers/accelerators.ts
+++ scripts/utils/helpers/accelerators.ts
@@ -1,31 +1,36 @@
 import { Account, Contract, Provider, TypedContractV2 } from "starknet";
 import { stringToHex } from "./common";
 import { optionRoundAbi, vaultAbi } from "../../abi";
-import { getNow, setAndMineNextBlock } from "../katana";
+import { getNow, mineNextBlock, setAndMineNextBlock } from "../katana";
 
 async function accelerateToAuctioning(
   provider: Provider,
   vaultContract: TypedContractV2<typeof vaultAbi>
 ) {
-  const optionRoundId = await vaultContract.current_option_round_id();
-  const optionRoundAddressDecimalString =
-    await vaultContract.get_option_round_address(optionRoundId);
-  const optionRoundAddressHexString: string =
-    "0x" + stringToHex(optionRoundAddressDecimalString);
+  try {
+    const optionRoundId = await vaultContract.current_option_round_id();
+    const optionRoundAddressDecimalString =
+      await vaultContract.get_option_round_address(optionRoundId);
+    const optionRoundAddressHexString: string =
+      "0x" + stringToHex(optionRoundAddressDecimalString);
 
-  const optionRoundContract = new Contract(
-    optionRoundAbi,
-    optionRoundAddressHexString,
-    provider
-  ).typedv2(optionRoundAbi);
+    const optionRoundContract = new Contract(
+      optionRoundAbi,
+      optionRoundAddressHexString,
+      provider
+    ).typedv2(optionRoundAbi);
 
-  const currentTime = await getNow(provider);
-  const auctionStartDate = await optionRoundContract.get_auction_start_date();
+    const currentTime = await getNow(provider);
+    const auctionStartDate = await optionRoundContract.get_auction_start_date();
 
-  await setAndMineNextBlock(
-    Number(auctionStartDate) - Number(currentTime),
-    provider.channel.nodeUrl
-  );
+    await setAndMineNextBlock(
+      provider,
+      Number(auctionStartDate) - Number(currentTime),
+      provider.channel.nodeUrl
+    );
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export { accelerateToAuctioning };
