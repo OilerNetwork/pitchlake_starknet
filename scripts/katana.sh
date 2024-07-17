f757ede672db82d4a3f02563a5d618e495f75286
mohiiit <mohit18343@iiitd.ac.in>
2024-07-18 04:12:19 +0530

    sleep removed from bash script

--- scripts/katana.sh
+++ scripts/katana.sh
@@ -6,9 +6,9 @@ PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.ge
 echo "Starting katana on port $PORT"
 katana --chain-id SN_SEPOLIA --host 127.0.0.1 --port 5050 --accounts "25" --seed "1" -b 2000 --dev &
 
-while ! nc -z localhost $PORT; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
+# while ! nc -z localhost $PORT; do   
+#   sleep 0.1 # wait for 1/10 of the second before check again
+# done
 
 # Run your Node script
 echo "Running main.js on port $PORT"
