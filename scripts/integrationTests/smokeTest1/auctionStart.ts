3e8f35b3b75349abc6c180f8b11b5f9b642801ff
Dhruv Gupta <dhruv035@gmail.com>
2024-07-18 08:17:59 +0530

    Bug fixes (#133)

--- scripts/integrationTests/smokeTest1/auctionStart.ts
+++ scripts/integrationTests/smokeTest1/auctionStart.ts
@@ -1,4 +1,4 @@
-import { Provider } from "starknet";
+import { CairoUint256, Provider, Uint256 } from "starknet";
 import { getAccount, getCustomAccount } from "../../utils/helpers/common";
 import { liquidityProviders, optionBidders } from "../../utils/constants";
 import { VaultFacade } from "../../utils/facades/vaultFacade";
@@ -6,7 +6,12 @@ import { EthFacade } from "../../utils/facades/ethFacade";
 import { setupOptionRound } from "../../utils/helpers/setup";
 import { OptionRoundFacade } from "../../utils/facades/optionRoundFacade";
 import assert from "assert";
-import { Constants } from "../../utils/facades/types";
+import {
+  ApprovalArgs,
+  Constants,
+  PlaceBidArgs,
+} from "../../utils/facades/types";
+import { mineNextBlock } from "../../utils/katana";
 export const smokeTest = async (
   provider: Provider,
   vaultFacade: VaultFacade,
@@ -45,6 +50,12 @@ export const smokeTest = async (
     optionBidders[1].account,
     optionBidders[1].privateKey
   );
+
+  const optionBidderC = getCustomAccount(
+    provider,
+    optionBidders[2].account,
+    optionBidders[2].privateKey
+  );
   assert(
     stateAfter.activeVariant() === "Open",
     `Expected:Open\nReceived:${stateAfter.activeVariant()}`
@@ -68,7 +79,112 @@ export const smokeTest = async (
   const totalUnlockedAmount = await vaultFacade.getTotalUnLocked();
 
   //Asserts
+  checkpoint1({
+    unlockedBalanceA,
+    unlockedBalanceB,
+    lockedBalanceA,
+    lockedBalanceB,
+    totalLockedAmount,
+    totalUnlockedAmount,
+    constants,
+  });
 
+  //Approve OptionBidders
+
+  const approveAllData: Array<ApprovalArgs> = [
+    {
+      owner: optionBidderA,
+      amount: BigInt("90000000000000000000"),
+      spender: optionRoundFacade.optionRoundContract.address,
+    },
+    {
+      owner: optionBidderB,
+      amount: BigInt("90000000000000000000"),
+      spender: optionRoundFacade.optionRoundContract.address,
+    },
+  ];
+  await ethFacade.approveAll(approveAllData);
+  await mineNextBlock(provider.channel.nodeUrl);
+
+  //Place bids according to story script
+  const reservePrice = await optionRoundFacade.getReservePrice();
+  const totalOptionAvailable =
+    await optionRoundFacade.getTotalOptionsAvailable();
+
+  const balanceBeforeBidA = await ethFacade.getBalance(optionBidderA.address);
+  const balanceBeforeBidB = await ethFacade.getBalance(optionBidderB.address);
+  console.log(
+    "reservePrice",
+    Number(reservePrice),
+    "\ntotalOptionsAvailable:",
+    totalOptionAvailable
+  );
+
+  const placeBidsData: Array<PlaceBidArgs> = [
+    {
+      from: optionBidderA,
+      amount: BigInt(totalOptionAvailable) / BigInt(2),
+      price: BigInt(3) * BigInt(reservePrice),
+    },
+    {
+      from: optionBidderB,
+      amount: BigInt(totalOptionAvailable) / BigInt(2),
+      price: BigInt(2) * BigInt(reservePrice),
+    },
+    {
+      from: optionBidderB,
+      amount: BigInt(totalOptionAvailable) / BigInt(2),
+      price: BigInt(reservePrice),
+    },
+  ];
+  await optionRoundFacade.placeBidsAll(placeBidsData);
+
+  const balanceAfterBidA = await ethFacade.getBalance(optionBidderA.address);
+  const balanceAfterBidB = await ethFacade.getBalance(optionBidderB.address);
+
+  const bidsForA = await optionRoundFacade.getBidsFor(optionBidderA.address);
+  const bidsForB = await optionRoundFacade.getBidsFor(optionBidderB.address);
+
+  checkpoint2({
+    balanceBeforeBidA,
+    balanceAfterBidA,
+    balanceBeforeBidB,
+    balanceAfterBidB,
+    reservePrice,
+    totalOptionAvailable,
+    bidsForA,
+    bidsForB,
+  });
+};
+
+async function checkpoint1({
+  lockedBalanceA,
+  lockedBalanceB,
+  unlockedBalanceA,
+  unlockedBalanceB,
+  totalLockedAmount,
+  totalUnlockedAmount,
+  constants,
+}: {
+  lockedBalanceA: bigint | number | Uint256 | undefined;
+  lockedBalanceB: bigint | number | Uint256 | undefined;
+  unlockedBalanceA: bigint | number | Uint256 | undefined;
+  unlockedBalanceB: bigint | number | Uint256 | undefined;
+  totalLockedAmount: bigint | number | Uint256 | undefined;
+  totalUnlockedAmount: bigint | number | Uint256 | undefined;
+  constants: Constants;
+}) {
+
+  const data = {
+    lockedBalanceA,
+    lockedBalanceB,
+    unlockedBalanceA,
+    unlockedBalanceB,
+    totalLockedAmount,
+    totalUnlockedAmount,
+    constants,
+  }
+  console.log("checkpoint1:\n",data)
   assert(
     Number(unlockedBalanceA) === 0,
     `UnlockedBalanceA 0 expected, found ${unlockedBalanceA}`
@@ -97,4 +213,65 @@ export const smokeTest = async (
     Number(totalLockedAmount) === constants.depositAmount,
     `Total Locked amount ${constants.depositAmount} expected, found ${totalLockedAmount}`
   );
-};
+}
+
+async function checkpoint2({
+  balanceBeforeBidA,
+  balanceAfterBidA,
+  balanceBeforeBidB,
+  balanceAfterBidB,
+  totalOptionAvailable,
+  reservePrice,
+  bidsForA,
+  bidsForB,
+}: {
+  balanceBeforeBidA: number | bigint;
+  balanceAfterBidA: number | bigint;
+  balanceBeforeBidB: number | bigint;
+  balanceAfterBidB: number | bigint;
+  totalOptionAvailable: number | bigint;
+  reservePrice: number | bigint;
+  bidsForA: Array<any>;
+  bidsForB: Array<any>;
+}) {
+  console.log("Bids from A:\n", bidsForA, "\nBids from B:\n", bidsForB);
+  assert(
+    BigInt(balanceBeforeBidA) - BigInt(balanceAfterBidA) ===
+      (BigInt(3) * BigInt(reservePrice) * BigInt(totalOptionAvailable)) /
+        BigInt(2),
+    "Error A"
+  );
+  assert(
+    BigInt(balanceBeforeBidB) - BigInt(balanceAfterBidB) ===
+      (BigInt(3) * BigInt(reservePrice) * BigInt(totalOptionAvailable)) /
+        BigInt(2),
+    "Error B"
+  );
+
+  assert(bidsForA.length === 1, "No. of Bids for A wrong");
+  assert(
+    bidsForA[0].amount === BigInt(totalOptionAvailable) / BigInt(2),
+    "Bid for A amount wrong"
+  );
+  assert(
+    bidsForA[0].price === BigInt(3) * BigInt(reservePrice),
+    "Bid for A price wrong"
+  );
+  assert(bidsForB.length === 2, "No. of Bids for B wrong");
+  assert(
+    bidsForB[1].amount === BigInt(totalOptionAvailable) / BigInt(2),
+    "First bid for B amount wrong"
+  );
+  assert(
+    bidsForB[1].price === BigInt(2) * BigInt(reservePrice),
+    "First bid for B price wrong"
+  );
+  assert(
+    bidsForB[0].amount === BigInt(totalOptionAvailable) / BigInt(2),
+    "Second bid for B amount wrong "
+  );
+  assert(
+    BigInt(bidsForB[0].price) === BigInt(reservePrice),
+    `Second bid for B price wrong.\n Expected:${reservePrice}, Actual:${bidsForB[1].price}`
+  );
+}
--- scripts/utils/facades/optionRoundFacade.ts
+++ scripts/utils/facades/optionRoundFacade.ts
@@ -1,51 +1,83 @@
-import { TypedContractV2 } from "starknet";
+import { CairoUint256, TypedContractV2 } from "starknet";
 import { optionRoundAbi } from "../../abi";
-import { PlaceBidArgs, UpdateBidArgs } from "./types";
+import { Bid, PlaceBidArgs, UpdateBidArgs } from "./types";
 
 export class OptionRoundFacade {
-    optionRoundContract:TypedContractV2<typeof optionRoundAbi>
-    
-    constructor(optionRoundContract:TypedContractV2<typeof optionRoundAbi>){
-        this.optionRoundContract = optionRoundContract
+  optionRoundContract: TypedContractV2<typeof optionRoundAbi>;
+
+  constructor(optionRoundContract: TypedContractV2<typeof optionRoundAbi>) {
+    this.optionRoundContract = optionRoundContract;
+  }
+
+  async getTotalOptionsAvailable() {
+    const res = await this.optionRoundContract.get_total_options_available();
+    if (typeof res !== "bigint" && typeof res !== "number") {
+      const data = new CairoUint256(res);
+      return data.toBigInt();
+    } else return res;
+  }
+  async getReservePrice() {
+    const res = await this.optionRoundContract.get_reserve_price();
+    if (typeof res !== "bigint" && typeof res !== "number") {
+      const data = new CairoUint256(res);
+      return data.toBigInt();
+    } else return res;
+  }
+  async getBidsFor(address: string) {
+    const res = await this.optionRoundContract.get_bids_for(address);
+    const bids: Array<Bid> = [];
+
+    for (let data of res) {
+      let amount: number | bigint;
+      let price: number | bigint;
+      if (typeof data.amount !== "bigint" && typeof data.amount !== "number") {
+        const res = new CairoUint256(data.amount);
+        amount = res.toBigInt();
+      } else {
+        amount = data.amount;
+      }
+
+      if (typeof data.price !== "bigint" && typeof data.price !== "number") {
+        const res = new CairoUint256(data.price);
+        price = res.toBigInt();
+      } else {
+        price = data.price;
+      }
+
+      const bid: Bid = {
+        id: data.id,
+        amount: amount,
+        nonce: data.nonce,
+        owner: data.owner,
+        price: price,
+        isTokenized: data.is_tokenized,
+        isRefunded: data.is_refunded,
+      };
+      bids.push(bid);
     }
-
-
-async updateBid (
-    { bidId, from, amount, price }: UpdateBidArgs,
-  ){
+    return bids;
+  }
+  async updateBid({ bidId, from, amount, price }: UpdateBidArgs) {
     this.optionRoundContract.connect(from);
     try {
       await this.optionRoundContract.update_bid(bidId, amount, price);
     } catch (err) {
       console.log(err);
     }
-  };
-  
-  
-async placeBid (
-    { from, beneficiary, amount, price }: PlaceBidArgs,
-  ){
+  }
+
+  async placeBid({ from, amount, price }: PlaceBidArgs) {
     this.optionRoundContract.connect(from);
     try {
-      await this.optionRoundContract.place_bid(amount, price, beneficiary);
+      const data = await this.optionRoundContract.place_bid(amount, price);
     } catch (err) {
       console.log(err);
     }
-  };
-  
-async placeBidAll (
-    placeBidData: Array<PlaceBidArgs>,
-    optionRoundContract: TypedContractV2<typeof optionRoundAbi>
-  ){
-  
-    for (const {from,amount,price,beneficiary} of placeBidData){
-      optionRoundContract.connect(from);
-          try {
-            await optionRoundContract.place_bid(amount, price, beneficiary);
-          } catch (err) {
-            console.log(err);
-          }
+  }
+
+  async placeBidsAll(placeBidData: Array<PlaceBidArgs>) {
+    for (const placeBidArgs of placeBidData) {
+      await this.placeBid(placeBidArgs);
     }
-  };
-  
+  }
 }
