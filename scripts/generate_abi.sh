3a3c64224b02d9d0b94c052709c1fc2591f13de3
Jithin KS <jithinks112@gmail.com>
2024-07-22 17:24:21 +0530

    fix scripts

--- scripts/generate_abi.sh
+++ scripts/generate_abi.sh
@@ -5,26 +5,11 @@
 contracts=(
     "OptionRound:optionRound"
     "Vault:vault"
+    "MarketAggregator:marketAggregator"
 )
 
-build_needed=false
-
-# Check if any JSON file is missing
-for contract in "${contracts[@]}"; do
-    IFS=':' read -r contract_name abi_name <<< "$contract"
-    json_file="../target/dev/pitch_lake_starknet_${contract_name}.contract_class.json"
-    
-    if [ ! -f "$json_file" ]; then
-        echo "Missing file: $json_file"
-        build_needed=true
-    fi
-done
-
-# Run scarb build if any file was missing
-if $build_needed; then
-    echo "Running scarb build..."
-    cd .. && scarb build && cd scripts
-fi
+echo "Running scarb build..."
+cd .. && scarb build && cd scripts
 
 # Generate ABIs
 for contract in "${contracts[@]}"; do
